31e4c10c0cfc6be03311ef2e8a12184467980c05
Anton <a.tashchilin@bk.ru>
2024-01-29 21:34:57 +0400

    delete loss func

--- src/main/java/com/ml/util/lossFunction/LossFunction.java
+++
@@ -1,9 +0,0 @@
-package com.ml.util.lossFunction;
-
-import java.util.function.BiFunction;
-
-import com.ml.util.linearAlgebra.Matrix;
-
-public interface LossFunction<T extends Number> extends BiFunction<Matrix<T>,Matrix<T>,Matrix<Double>>  {
-    
-}
