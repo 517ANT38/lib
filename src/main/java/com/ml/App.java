154a210a6f296f64de176834282d61225efa28a6
Anton <a.tashchilin@bk.ru>
2024-01-30 17:38:48 +0400

    test first net

--- src/main/java/com/ml/App.java
+++ src/main/java/com/ml/App.java
@@ -1,5 +1,13 @@
 package com.ml;
 
+import java.util.List;
+
+import com.ml.net.Net;
+import com.ml.optimizer.Optimizer;
+import com.ml.optimizer.SGD;
+import com.ml.util.activationFunction.SingleLeap;
+import com.ml.util.linearAlgebra.MatArray;
+import com.ml.util.lossFunction.MeanSquarErr;
 /**
  * Hello world!
  *
@@ -8,6 +16,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Net net = new Net(new SingleLeap(),2,3,1,1);
+        
+        Optimizer opt = new SGD(100, 0.12, 0.11, new MeanSquarErr());
+        opt.opt(net, new MatArray(new double[][]{{0,1},{1,1},{0,0},{1,0}}),
+            new MatArray(new double[][]{{1},{0},{0},{1}}));
+        System.out.println(net.getResult(new MatArray(new double[][]{{1,1}})));
     }
 }
