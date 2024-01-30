c8d88d39c4e08fbe467ab0411e2a7ac3cd696602
Anton <a.tashchilin@bk.ru>
2024-01-30 14:33:19 +0400

    update grad output layer

--- src/main/java/com/ml/net/layer/LayerOutput.java
+++ src/main/java/com/ml/net/layer/LayerOutput.java
@@ -1,13 +1,15 @@
 package com.ml.net.layer;
 
+import java.io.Serializable;
+
 import com.ml.util.activationFunction.ActivationFunction;
 import com.ml.util.linearAlgebra.MatArray;
 import com.ml.util.linearAlgebra.Matrix;
 import com.ml.util.randomGenMatrix.RandomGenerator;
 import com.ml.util.randomGenMatrix.RandomGeneratorR;
 
-public class LayerOutput implements Layerable{
-
+public class LayerOutput implements Layerable, Serializable{
+    private static final long serialVersionUID = 1L;
     private Matrix<Double> matrix;
     private Matrix<Double> biases;
     private Matrix<Double> y;
@@ -30,11 +32,11 @@ public class LayerOutput implements Layerable{
     @Override
     public Matrix<Double> back(Matrix<Double> m, double coff) {
         var d = this.y.sub(m).map(x -> func.difApply(x));
-        this.matrix = matrix.add(matrix
+        this.matrix = matrix.add(y
             .transpose()
             .dot(d)
             .map(x -> x * coff));
-        this.biases = biases.add()// научиться находить сумму элементов строки (столбца)
+        this.biases = biases.map(x -> x + d.sum(0, 0)*coff);// научиться находить сумму элементов строки (столбца)
         return d;
     }
 
