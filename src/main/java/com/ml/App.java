476ecfab383ee691a1518890501e0e640c25a652
Anton <a.tashchilin@bk.ru>
2024-02-01 09:08:26 +0400

    check test

--- src/main/java/com/ml/App.java
+++ src/main/java/com/ml/App.java
@@ -16,15 +16,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        Net net = new Net(new LogSigmoid(),2,34,1,3);
+        Net net = new Net(new LogSigmoid(),3,34,1,3);
         
         Optimizer opt = new SGD(8, 0.000000001, 0.1, new MeanSquarErr());
 
-        System.out.println(net.getResult(new MatArray(new double[][]{{0,1}})));
+        System.out.println(net.getResult(new MatArray(new double[][]{{0,1,1}})));
         opt.opt(net, new double[][]{
-            {0,0},{1,0},{0,1},{1,1}
+            {0,0,0},{1,0,1},{0,1,1},{1,1,1}
         },new double[][]{{0,0,0},{1,1,1},{1,1,1},{0,0,0}});
-        System.out.println(net.getResult(new MatArray(new double[][]{{0,1}})));
+        System.out.println(net.getResult(new MatArray(new double[][]{{0,1,1}})));
        
         // net.serialization("net");
         // var net1 = Net.readCreateNet("net");
