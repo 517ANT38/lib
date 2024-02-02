8e49e6ef20058b318e117e030a78cd2bd36438af
Anton <a.tashchilin@bk.ru>
2024-02-02 21:58:05 +0400

    add opt Adagrad

---
+++ src/main/java/com/ml/optimizer/util/Adagrad.java
@@ -0,0 +1,45 @@
+package com.ml.optimizer.util;
+
+import com.ml.util.linearAlgebra.Matrix;
+
+public class Adagrad implements Optimizer {
+
+    private Matrix<Double> grad;
+    private double rate;
+    private double iner;
+    private double ep;
+
+    public Adagrad(double rate, double iner,double ep) {
+        this.rate = rate;
+        this.iner = iner;
+        this.ep = ep;
+    }
+
+    public Adagrad(double rate, double iner){
+        this(rate,iner,10e-6);
+    }
+
+    @Override
+    public Matrix<Double> optWs(Matrix<Double> ws, Matrix<Double> d, Matrix<Double> y) {
+        var dw = y.mult(d);
+        var g = dw;
+        if(grad != null){            
+            dw = grad.map(x -> x * iner)
+                .add(dw.map(x -> x * x * (1 - iner)));
+        }
+        this.grad = dw;
+        return ws.sub(grad.map(x -> Math.sqrt(x + ep))
+            .divide(g.map(x -> x * rate)));
+    }
+
+    @Override
+    public Matrix<Double> optBs(Matrix<Double> bs, Matrix<Double> d) {
+        return bs.map(x -> x + d.sum(0, 0)*(-rate));
+    }
+
+    @Override
+    public void cleanState() {
+        this.grad = null;
+    }
+    
+}
