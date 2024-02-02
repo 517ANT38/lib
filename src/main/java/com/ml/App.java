cbedcaf0b0089ccfc4f165778eff3027b624ea6f
Anton <a.tashchilin@bk.ru>
2024-02-02 15:21:31 +0400

    test rmsprop

--- src/main/java/com/ml/App.java
+++ src/main/java/com/ml/App.java
@@ -5,21 +5,32 @@ package com.ml;
 import java.util.List;
 
 import com.ml.net.Net;
-import com.ml.optimizer.Optimizer;
-import com.ml.optimizer.RMSProp;
-import com.ml.optimizer.SGD;
+import com.ml.net.layer.LayerHidden;
+import com.ml.net.layer.LayerOutput;
+import com.ml.net.layer.Layerable;
+import com.ml.optimizer.OptIter;
+import com.ml.optimizer.OptIterIpml;
+import com.ml.optimizer.util.RMSProp;
+import com.ml.optimizer.util.SGD;
 import com.ml.util.activationFunction.LogSigmoid;
-import com.ml.util.activationFunction.SingleLeap;
+import com.ml.util.activationFunction.Sin;
 import com.ml.util.linearAlgebra.MatArray;
 import com.ml.util.lossFunction.MeanSquarErr;
+import com.ml.util.randomGenMatrix.RandomGeneratorGaussian;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        Net net = new Net(new LogSigmoid(),3,34,1,3);
+        var rg = new RandomGeneratorGaussian();
+        var func = new LogSigmoid();
+        Layerable layerInpt = new LayerHidden(3, 2, func , new RMSProp(0.11, 0.1), rg);
+        Layerable layerHid = new LayerHidden(2, 2, func , new RMSProp(0.11, 0.1), rg);
+        Layerable layerOut = new LayerOutput(2, 3, func , new RMSProp(0.11, 0.1), rg);
+        var lst = List.of(layerInpt,layerHid,layerOut);
+        Net net = new Net(lst);
         
-        Optimizer opt = new RMSProp(8, 0.000000001, 0.11,0.1, new MeanSquarErr());
+        OptIter opt = new OptIterIpml(1000,0.01, new MeanSquarErr());
 
         System.out.println(net.getResult(new MatArray(new double[][]{{0,1,1}})));
         opt.opt(net, new double[][]{
