b10144eb1d4dd84158243961375f4ee742e6d509
Anton <a.tashchilin@bk.ru>
2024-02-14 17:38:41 +0400

    learn net num choise

--- src/main/java/com/ml/App.java
+++ src/main/java/com/ml/App.java
@@ -2,56 +2,55 @@ package com.ml;
 
 
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-
-import javax.imageio.ImageIO;
 
 import com.ml.net.Net;
+import com.ml.net.Netable;
 import com.ml.net.layer.LayerHidden;
 import com.ml.net.layer.LayerOutput;
 import com.ml.net.layer.Layerable;
 import com.ml.optimizer.OptIter;
 import com.ml.optimizer.OptIterIpml;
-import com.ml.optimizer.util.Adagrad;
-import com.ml.optimizer.util.MomentumSGD;
-import com.ml.optimizer.util.NAG;
 import com.ml.optimizer.util.SGD;
 import com.ml.util.activationFunction.LogSigmoid;
-import com.ml.util.activationFunction.Sin;
-import com.ml.util.linearAlgebra.MatArray;
+import com.ml.util.dataNormolize.Normolizable;
+import com.ml.util.dataNormolize.NormolizableIml;
 import com.ml.util.lossFunction.MeanSquarErr;
 import com.ml.util.randomGenMatrix.RandomGeneratorGaussian;
+import com.ml.util.readWriteDataSet.ImageRead;
 
 public class App 
 {
     public static void main( String[] args ) throws IOException
     {
-        // var rg = new RandomGeneratorGaussian();
-        // var func = new LogSigmoid();
-        // Layerable layerInpt = new LayerHidden(3, 5, func , new Adagrad(0.11, 0.1), rg);
-        // Layerable layerHid = new LayerHidden(5, 5, func , new Adagrad(0.11, 0.1), rg);
-        // Layerable layerOut = new LayerOutput(5, 3, func , new Adagrad(0.11, 0.1), rg);
-        // var lst = List.of(layerInpt,layerHid,layerOut);
-        // Net net = new Net(lst);
+        var imageRead = new ImageRead();
+        Normolizable normalizer = new NormolizableIml(); 
+        double[][] set = new double[10][];
+        double[][] res = new double[10][10];
         
-        // OptIter opt = new OptIterIpml(1000,0.01, new MeanSquarErr());
+        for (int i = 0; i < 9; i++) {            
 
-        // System.out.println(net.getResult(new MatArray(new double[][]{{0,1,1}})));
-        // opt.opt(net, new double[][]{
-        //     {0,0,0},{1,0,1},{0,1,1},{1,1,1}
-        // },new double[][]{{0,0,0},{1,1,1},{1,1,1},{0,0,0}});
-        // System.out.println(net.getResult(new MatArray(new double[][]{{0,1,1}})));
-       
-        // net.serialization("net");
-        // var net1 = Net.readCreateNet("net");
-        // System.out.println(net.getResult(new MatArray(new double[][]{{0,1}})));
-        
+            set[i] = imageRead.read("example/"+i+".jpg");
+            set[i] = normalizer.normolize(set[i]);
+        }
+        for (int i = 0; i < res.length; i++) {
+            res[i][i]=1;
+        }
+        var sgd = new SGD(0.11);
+        var rg = new RandomGeneratorGaussian();
+        var func = new LogSigmoid();
+
+        Layerable input = new LayerHidden(set[0].length, 5, func,sgd,rg);
+        Layerable hidden = new LayerHidden(5, 5, func,sgd,rg);
+        Layerable out = new LayerOutput(5, 10, func, sgd, rg);
+
+        Netable net = new Net(List.of(input,hidden,out));
+
+        OptIter optIter = new OptIterIpml(100, 0.4, new MeanSquarErr());
+
+        optIter.opt(net, set, res);
+        net.serialization("net");
     }
     
 }
